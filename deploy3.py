fb481118658cf1b8a02e8066ee6be3457ebda498
Bindu Hegde <binduhegde1932@gmail.com>
2024-05-23 10:46:05 +0530

    improve css

--- deploy3.py
+++ deploy3.py
@@ -9,50 +9,53 @@ np.bool = np.bool_
 np.object = object
 
 
+# to style the heading of every step.
+# puts a yellow circle at LHS and the heaidng will overlap
+# just for aesthetics
 def style_heading(text):
     left = """<div style="position: relative; font-family: Arial, sans-serif; font-size: 20px; display: inline-block; margin-top: 50px; margin-bottom: 50px; margin-left: 5px;">
-    <span style="position: absolute; top: 50%; left: 0; transform: translate(-50%, -50%); width: 70px; height: 70px; background-color: #F9C58D; border-radius: 50%;"></span>
+    <span style="position: absolute; top: 50%; left: 0; transform: translate(-50%, -50%); width: 90px; height: 90px; background-color: #F9C58D; border-radius: 50%;"></span>
     <span style="position: relative; left: 5px;">"""
     right = """</span>
 </div>"""
     return left + text + right
 
-# def style_list(index, heading, items):
-#     result = f"""<div style="position: relative; font-family: Arial, sans-serif; font-size: 20px; display: inline-block; margin-top: 50px; margin-bottom: 50px; margin-left: 5px;">
-#     <span style="position: absolute; top: 50%; left: 0; transform: translate(-50%, -50%); width: 70px; height: 70px; background-color: #F9C58D; border-radius: 50%;">{index}</span>
-#     <span style="position: relative; left: 5px;">{heading}</span></div>
-#     """
 
-
+# to style the steps taken by the user. returns html string with css included
+# puts the index number
 def create_styled_div(index, heading, items):
     items_html = ''.join(
         [f"<div style='border-radius: 5px; margin-right: 5px; color: white; padding-left: 3px; padding-right: 3px; font-size: 12px; font-family: IBM Plex Sans, sans-serif; line-height: 1.6; background-color: #ff0066; padding-left: 15px; padding-right: 15px; padding-top: 2px; padding-bottom: 2px;'>{item}</div>" for item in items])
     return f"""
-    <div style='border: 1px solid #ff0066; padding: 20px; border-radius: 10px; max-width: 600px; position: relative; font-family: Arial, sans-serif; margin-bottom: 20px; margin-left: 5%;'>
-        <div style='position: absolute; top: 20%; left: 5%; width: 30px; height: 30px; background-color: #ff0066; border-radius: 50%; display: flex; align-items: center; justify-content: center; color: white; font-weight: bold;'>
+    <div style='padding: 20px; border-radius: 10px; max-width: 600px; position: relative; font-family: Arial, sans-serif; margin-bottom: 20px; margin-left: 5%; box-shadow: 0.1rem 0.1rem 1.5rem rgba(0, 0, 0, 0.3);'>
+        <div style='position: absolute; width: 600px; height: 10px; background-color: #F9C58D; align-items: center; left: 0px; top: 0px;'></div>
+        <div style='position: absolute; top: 30%; left: 5%; width: 50px; height: 50px; background-color: #F9C58D; border-radius: 50%; display: flex; align-items: center; justify-content: center; color: white; font-weight: bold; padding: 5px;'>
             {index}
         </div>
-        <div style='margin-left: 10%;'>
+        <div style='margin-left: 15%; margin-top: 10px;'>
             <h3 style='margin: 0;'>{heading}</h3>
         </div>
-        <div style='margin-top: 20px; display: flex; flex-wrap: wrap;'>
+        <div style='margin-top: 30px; display: flex; flex-wrap: wrap; margin-left: 90px;'>
             {items_html}
         </div>
     </div>
     """
-
-
-def style_subtext(text):
-    return f"<p style='font-family: Arial, sans-serif; font-size: 16px; margin-bottom: 10px;'>{text}</p>"
-
-
-def style_list(items):
-    return f"<ul style='font-family: Arial, sans-serif; font-size: 16px; margin-bottom: 20px;'>{''.join([f'<li>{item}</li>' for item in items])}</ul>"
-
-# step1 = """<div style="position: relative; font-family: Arial, sans-serif; font-size: 20px; display: inline-block; margin-top: 50px; margin-bottom: 50px; margin-left: 5px;">
-#     <span style="position: absolute; top: 50%; left: 0; transform: translate(-50%, -50%); width: 70px; height: 70px; background-color: #F9C58D; border-radius: 50%;"></span>
-#     <span style="position: relative; left: 5px;">Step 1: Handling Missing Values</span>
-# </div>"""
+# def create_styled_div(index, heading, items):
+#     items_html = ''.join(
+#         [f"<div style='border-radius: 5px; margin-right: 5px; color: white; padding-left: 3px; padding-right: 3px; font-size: 12px; font-family: IBM Plex Sans, sans-serif; line-height: 1.6; background-color: #ff0066; padding-left: 15px; padding-right: 15px; padding-top: 2px; padding-bottom: 2px;'>{item}</div>" for item in items])
+#     return f"""
+#     <div style='border: 1px solid #ff0066; padding: 20px; border-radius: 10px; max-width: 600px; position: relative; font-family: Arial, sans-serif; margin-bottom: 20px; margin-left: 5%; box-shadow: 0.1rem 0.1rem 1.5rem rgba(0, 0, 0, 0.3);'>
+#         <div style='position: absolute; top: 20%; left: 5%; width: 30px; height: 30px; background-color: #ff0066; border-radius: 50%; display: flex; align-items: center; justify-content: center; color: white; font-weight: bold;'>
+#             {index}
+#         </div>
+#         <div style='margin-left: 10%;'>
+#             <h3 style='margin: 0;'>{heading}</h3>
+#         </div>
+#         <div style='margin-top: 20px; display: flex; flex-wrap: wrap;'>
+#             {items_html}
+#         </div>
+#     </div>
+#     """
 
 
 class StreamlitApp:
@@ -60,7 +63,6 @@ class StreamlitApp:
         title = '<div style="background: linear-gradient(to right,#F9C58D,#F492F0); color: transparent; color: black; font-family: Helvetica; font-weight: bold; font-size: 40px; text-align: center; padding: 30px; letter-spacing:1px; width: 100%; margin-bottom: 20px;"><h2>Machine Learning Preprocessor</h2></div>'
         st.markdown(title, unsafe_allow_html=True)
 
-        # st.title('Machine Learning Preprocessing')
         self.df_file = st.file_uploader('Upload the Dataframe', type=['csv'])
 
         if self.df_file is not None:
@@ -75,15 +77,10 @@ class StreamlitApp:
         if st.button('review my actions'):
             self.review_actions()
 
-        # st.markdown('---')  # Add a horizontal line
-        # if st.button('Download the preprocessed file'):
-        #     self.submit_clicked()
-
     def handle_null_values(self):
         step1 = style_heading('Step 1: Handling Misisng Values')
         st.markdown(step1, unsafe_allow_html=True)
 
-        # st.subheader('Step 1: Handling Null Values')
         st.text('These are the description of the columns with missing values')
         # Get columns with at least one missing value
         columns_with_na = [
@@ -155,7 +152,6 @@ class StreamlitApp:
         step3 = style_heading('Step 3: Feature Scaling')
         st.markdown(step3, unsafe_allow_html=True)
 
-        # st.subheader('Step 3: Feature Scaling')
         st.text('These are the numerical columns')
         num_cols = self.df.select_dtypes(exclude='object').columns
 
@@ -235,8 +231,6 @@ class StreamlitApp:
             data=csv,
             file_name='preprocessed_data.csv',
             mime='text/csv',)
-        # self.review_actions()
-        # st.subheader('The following actions will be taken')
 
     def set_user_pref(self):
         def exclude_columns(selected_cols, *exclusions):
